c79e8e9056563a586d53aa4d0c057e2223e441c0
Jose Waldo <josferw@gmail.com>
2024-01-10 09:38:43 -0500

    Style: cambie las columnas a 2

--- src/modules/pokemon/pokemon-list.tsx
+++ src/modules/pokemon/pokemon-list.tsx
@@ -3,7 +3,7 @@ import { type Pokemon } from "../../services/pokemon.service";
 
 const PokemonList = ({ pokemones }: { pokemones: Pokemon[] }) => {
   return (
-    <div className="grid grid-cols-3 place-items-center gap-4">
+    <div className="grid grid-cols-2 place-items-center gap-4">
       {pokemones?.map((pokemon) => (
         <PokemonCard
           key={pokemon.name}
