5c1983cbe69db057840e9f7f5a57f9c6ac344b5c
Jose Waldo <josferw@gmail.com>
2024-01-10 10:00:30 -0500

    Feat: se añadio una pagina de inicio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
-import Header from "../components/header";
+import HeroSection from "../components/hero";
+import Layout from "../layouts/layout";
 
 const Index = () => {
   return (
-    <main className="min-h-screen py-4 flex flex-col items-center gap-2">
-      <Header />
-      <section className="w-3/4 bg-base-200 rounded-lg p-4">
-        <p>algo</p>
-        <p>algo</p>
-        <p>algo</p>
-        <p>algo</p>
-      </section>
-    </main>
+    <Layout>
+      <HeroSection />
+    </Layout>
   );
 };
 
